7668670f8e0f7e0b9f345c63c1e0ddb197a5cc37
91790 <91790@LAPTOP-47DQICJU>
2023-10-25 21:39:02 +0530

    my first commit

---
+++ src/main/java/scannerProject/ScannerExample.java
@@ -0,0 +1,24 @@
+package scannerProject;
+import java.util.Scanner;
+public class ScannerExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Name ");
+		String name=sc.nextLine();
+		System.out.println("Name="+name);
+		
+		
+		System.out.println("Enter Age ");
+		int age=sc.nextInt();
+		System.out.println("Age="+age);
+		Scanner sca=new Scanner(System.in);
+		System.out.println("Enter Designation ");
+		String desig=sca.nextLine();
+		System.out.println("Designation="+desig);
+		
+
+	}
+
+}
